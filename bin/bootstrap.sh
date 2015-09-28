257638f9101b32d1e183b022965e19a3ca6e9a44
Kian-Meng, Ang <kianmeng@users.noreply.github.com>
2015-09-29 01:49:08 +0800

    Refactor steps into functions

--- bin/bootstrap.sh
+++ bin/bootstrap.sh
@@ -4,27 +4,31 @@
 # way to bypass this. Currently only RPM-based and DEB-based GNU/Linux distros
 # are supported.
 
-if [[ $(python -mplatform | grep -Ei 'ubuntu|debian') ]];
-then
-    sudo apt-add-repository -y ppa:ansible/ansible
-    sudo apt-get update
-    sudo apt-get install -y ansible git
-
+function git_clone_and_ansible_ping() {
     git clone https://github.com/kianmeng/dotfiles-ansible.git
     cd dotfiles-ansible
     ansible all -m ping
-elif [[ $(python -mplatform | grep -Ei 'fedora|centos') ]];
-then
-    if [[ $(id -u) -ne 0 ]];
+}
+
+function install_ansible_and_git() {
+    if [[ $(python -mplatform | grep -Ei 'ubuntu|debian') ]];
     then
-        su -
+        sudo apt-add-repository -y ppa:ansible/ansible
+        sudo apt-get update
+        sudo apt-get install -y ansible git
+    elif [[ $(python -mplatform | grep -Ei 'fedora|centos') ]];
+    then
+        if [[ $(id -u) -ne 0 ]];
+        then
+            su -
+        else
+            yum install epel-release -y
+            yum install ansible git -y
+        fi
     else
-        yum install epel-release -y
-        yum install ansible git -y
-        git clone https://github.com/kianmeng/dotfiles-ansible.git
-        cd dotfiles-ansible
-        ansible all -m ping
+        echo -e "Error: Only RPM-based or DEB-based GNU/Linux distros are supported.\n"
+        exit 1
     fi
-else
-    echo -e "Error: Only RPM-based or DEB-based GNU/Linux distros are supported.\n"
-fi
+}
+
+install_ansible_and_git && git_clone_and_ansible_ping
