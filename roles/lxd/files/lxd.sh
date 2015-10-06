5f213104ab0e0bc192422321fb5b8eda06c9966b
Kian-Meng, Ang <kianmeng@users.noreply.github.com>
2015-10-07 02:12:09 +0800

    Add LXD alias/function for Bash

---
+++ roles/lxd/files/lxd.sh
@@ -0,0 +1,20 @@
+# Is better to use this to trace down issue as lxc and lxd sometimes just
+# freeze there.
+function lxcd() {
+    if [[ $# -eq 0 ]]; then
+        lxc
+    else
+        lxc $1 --debug --verbose "${@:2}"
+    fi
+}
+
+# Enter the LXC container and launch Bash shell.
+function lxcsh() {
+    if [[ $# -eq 0 ]]; then
+        echo "ERR: Missing container name. Cannot launch Bash Shell!"
+    else
+        lxcd exec $1 /bin/bash
+    fi
+}
+
+alias lxcls='lxc list'
