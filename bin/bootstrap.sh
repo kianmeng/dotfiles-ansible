1c477ca28912df948283ddc00358fa4a2a78d8cc
Kian-Meng, Ang <kianmeng@users.noreply.github.com>
2015-10-28 08:11:08 -0400

    Fix missing Python 2.7.x missing in Fedora 23
    Also set the default cloned path for the project.

--- bin/bootstrap.sh
+++ bin/bootstrap.sh
@@ -4,19 +4,28 @@
 # way to bypass this. Currently only RPM-based and DEB-based GNU/Linux distros
 # are supported.
 
+# Newer distro like Fedora 23 does not install Python 2.X by default as they
+# fully embraced Python 3.
+DEFAULT_PYTHON='python'
+[ -n $(command -v python) ] && DEFAULT_PYTHON='python3'
+
+DEFAULT_REPO_PATH="$HOME/Projects"
+
 function git_clone_and_ansible_ping() {
+    mkdir $DEFAULT_REPO_PATH
+    cd $DEFAULT_REPO_PATH
     git clone https://github.com/kianmeng/dotfiles-ansible.git
     cd dotfiles-ansible
     ansible all -m ping
 }
 
 function install_ansible_and_git() {
-    if [[ $(python -mplatform | grep -Ei 'ubuntu|debian') ]];
+    if [[ $($DEFAULT_PYTHON -mplatform | grep -Ei 'ubuntu|debian') ]];
     then
         sudo apt-add-repository -y ppa:ansible/ansible
         sudo apt-get update
         sudo apt-get install -y ansible git make
-    elif [[ $(python -mplatform | grep -Ei 'fedora|centos') ]];
+    elif [[ $($DEFAULT_PYTHON -mplatform | grep -Ei 'fedora|centos') ]];
     then
         if [[ $(id -u) -ne 0 ]];
         then
